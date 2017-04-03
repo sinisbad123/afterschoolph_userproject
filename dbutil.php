9aef20d3ca1d4e5cb68173e75680c495ce717cc7
Marcus Ang <ang.marcus.incosys@gmail.com>
2017-04-03 21:57:25 +0800

    Initial UI

---
+++ dbutil.php
@@ -0,0 +1,10 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "afterschoolph";
+
+//Connect to database assuming there will be no problem in the connection
+mysql_connect($host,$user,$password);
+mysql_select_db($datbase);
+?>
---
+++ search.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>AfterschoolPH | User Management System</title>
+
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+    <div class="header">
+    </div>
+
+    <div class="row search-panel">
+      <div class="col-lg-6 col-lg-offset-2">
+        <input type="text" class="form-control font-awesome-input" name="search_username" placeholder="&#xF002; Search Username" />
+      </div>
+
+      <div class="col-lg-2">
+        <input type="submit" name="search" class="btn btn-info btn-block" value="Search" />
+      </div>
+    </div>
+
+    <div class="row search-content">
+      <div class="col-lg-8 col-lg-offset-2">
+        <table class="table table-hover table-bordered hidden-xs hidden-sm">
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Date and Time started</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Build UI for AfterschoolPH test</td>
+              <td>04/03/2017 19:08:47</td>
+              <td>Pending</td>
+            </tr>
+
+            <tr>
+              <td>Create database schema for normalization</td>
+              <td>04/04/2017 17:53:20</td>
+              <td>Pending</td>
+            </tr>
+
+            <tr>
+              <td>Build log-in functionality</td>
+              <td>04/04/2017 19:32:24</td>
+              <td>Pending</td>
+            </tr>
+            
+          </tbody>
+        </table>
+
+        <table class="table table-striped visible-xs visible-sm">
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Date and Time started</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Build UI for AfterschoolPH test</td>
+              <td>04/03/2017 19:08:47</td>
+              <td>Pending</td>
+            </tr>
+
+            <tr>
+              <td>Create database schema for normalization</td>
+              <td>04/04/2017 17:53:20</td>
+              <td>Pending</td>
+            </tr>
+
+            <tr>
+              <td>Build log-in functionality</td>
+              <td>04/04/2017 19:32:24</td>
+              <td>Pending</td>
+            </tr>
+            
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+      <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  		<script type="text/javascript" src="js/app.js"></script>
+	</body>
+</html>
